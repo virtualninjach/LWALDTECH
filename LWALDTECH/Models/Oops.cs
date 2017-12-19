e4e62eece8eadcaba8060044b6a03523bb441505
chris h <chris@lwaldtech.com>
2017-12-19 15:51:25 -0500

    init

---
+++ LWALDTECH/Models/Oops.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LWALDTECH.Models
+{
+    public class Oops
+    {
+        public int ID { get; set; }
+        public string EventType { get; set; }
+        public string Action { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- LWALDTECH/Scripts/wowAPICalls.js
+++ LWALDTECH/Scripts/wowAPICalls.js
@@ -3,21 +3,21 @@
         type: "GET",
         headers: "*",
         methods: "*",
-        url: "http://us.battle.net/api/wow/data/character/races?locale=en_US",
+        url: "https://us.api.battle.net/wow/data/character/races?locale=en_US&apikey=6sunaaqxws2epqvznrwra26a5mtz6pf8",
         data: "{}",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (response) {
             var races = response.races;
             $('#output').empty();
-            $('#output').append('<strong>All World of Warcraft Races for US Locals</strong>')
+            $('#output').append('<strong>All World of Warcraft Races for US Locals</strong>');
             $.each(races, function (index, race) {
                 //returned json looks similiar to the below
                 //id":1,"mask":1,"side":"alliance","name":"H
               
-                $('#output').append('<table><tr>')
+                $('#output').append('<table><tr>');
                 $('#output').append('<td>id:' + race.id + '</td><td>mask:' + race.mask + '</td><td> Side: ' + race.side +'</td><td>name:' + race.name + '</td></tr>');
-                $('#output').append('</table>')
+                $('#output').append('</table>');
             });
         },
         failure: function (msg) {
@@ -26,27 +26,57 @@
     });
 }
 
-function getAllRacesEUGreatBritian() {
+function getAllRacesEU() {
     $.ajax({
         type: "GET",
         headers: "*",
         methods: "*",
-        url: "http://eu.battle.net/api/wow/data/character/races?locale=en_GB",
+        url: "https://eu.api.battle.net/wow/data/character/races?locale=en_GB&apikey=6sunaaqxws2epqvznrwra26a5mtz6pf8",
         data: "{}",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (response) {
             var races = response.races;
             $('#output').empty();
-            $('#output').append('<strong>All World of Warcraft Races for Great Britian</strong>')
+            $('#output').append('<strong>All World of Warcraft Races for EU</strong>');
             $.each(races, function (index, race) {
                 //returned json looks similiar to the below
                 //id":1,"mask":1,"side":"alliance","name":"H
                
-                $('#output').append('<table><tr>')
+                $('#output').append('<table><tr>');
                 $('#output').append('<td>id:' + race.id + '</td><td>mask:' + race.mask + '</td><td> Side: ' + race.side + '</td><td>name:' + race.name + '</td></tr>');
-                $('#output').append('</table>')
+                $('#output').append('</table>');
             });
+        },
+        failure: function (msg) {
+            $('#output').text(msg);
+        }
+    });
+}
+
+function getWoWAchievment() {
+    $.ajax({
+        type: "GET",
+        headers: "*",
+        methods: "*",
+        url: "https://us.api.battle.net/wow/character/firetree/otissius?fields=stats&locale=en_US&apikey=6sunaaqxws2epqvznrwra26a5mtz6pf8",
+        data: "{}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            var res = response;
+            $('#output').empty();
+            $('#output').append('<strong>Character Info</strong>');
+            $('#output').append('<li>Character Name: '+res.name);
+            $('#output').append('<li>Character Health: '+res.stats.health);
+            //$.each(res, function (index, r) {
+                //returned json looks similiar to the below
+                //id":1,"mask":1,"side":"alliance","name":"H
+
+             //   $('#output').append('<table><tr>');
+             //   $('#output').append('<td>id:' + r.stats.health + '</td></tr>');
+             //   $('#output').append('</table>');
+            //});
         },
         failure: function (msg) {
             $('#output').text(msg);
